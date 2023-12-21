5b4094956452032e33f1b3ca7d4e534b277b7f44
standec101 <okpalastandec@gmail.com>
2023-12-22 00:08:27 +0100

    new updaate

---
+++ index.js
@@ -0,0 +1,12 @@
+let show = false;
+const toggle= document.querySelector("#toggle");
+const password = document.querySelector("#password")
+
+toggle.addEventListener("click", () => {
+    if(show) {
+        password.setAttribute("type", "password");
+    } else {
+        password.setAttribute("type", "text");
+    }
+    show = !show
+})
